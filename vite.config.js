0e602e51a3658ce5d2acdf2459ed2e8f2a8c3296
zzz664 <temperis0512@gmail.com>
2026-03-30 22:11:57 +0900

    chore(vite.config.js): rollupOption 추가
    landing, signup, roomsDetail entry point 추가
    
    resolves #23

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        landing: resolve(__dirname, 'index.html'),
+        signup: resolve(__dirname, 'signup/index.html'),
+        roomsDetail: resolve(__dirname, 'rooms-detail/index.html'),
+      },
+    },
+  },
 });
