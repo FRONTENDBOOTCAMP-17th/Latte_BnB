c598e69aa88cc35f93b2032a1a550accb14a51c9
zzz664 <temperis0512@gmail.com>
2026-03-31 23:12:38 +0900

    refactor(toast): message, warn, success 3개의 기본 테마를 추가
    그리고 기존 메세지노드를 빌드해서 보여주는 과정을 하나의 함수에서 처리했는데,
    다양한 테마 제공을 위해 빌드, 보여줌 단계로 분리하여 중간에 빌드한 후 색상을 변경할 수 있도록 수정함

--- src/components/toast.js
+++ src/components/toast.js
@@ -9,20 +9,20 @@ function buildDefaultToast() {
   toastNode = document.createElement('div');
   toastNode.id = 'toast';
   toastNode.className =
-    'absolute top-4/5 left-1/2 -translate-y-full -translate-x-1/2 rounded-2xl p-4 border-1 border-negative-600 bg-negative-300 text-shark-700 shadow-md';
-  toastNode.appendChild(buildMessage());
-  toastNode.appendChild(buildDetail());
+    'absolute top-4/5 left-1/2 -translate-y-full -translate-x-1/2 rounded-2xl p-4 border-1 border-neutral-600 bg-neutral-300 text-shark-700 shadow-md';
+  toastNode.appendChild(buildMessageNode());
+  toastNode.appendChild(buildDetailNode());
   return toastNode;
 }
 
-function buildMessage() {
+function buildMessageNode() {
   const messageNode = document.createElement('p');
   messageNode.id = 'toastMessage';
   messageNode.className = 'font-semibold';
   return messageNode;
 }
 
-function buildDetail() {
+function buildDetailNode() {
   const detailNode = document.createElement('p');
   detailNode.id = 'toastDetail';
   detailNode.className = 'text-sm';
@@ -43,7 +43,16 @@ function attachTransitionEndEvent() {
   });
 }
 
-function show(message, details = '', duration = 2) {
+function show(duration) {
+  toastNode.showPopover();
+  toastNode.classList.remove('closeToast');
+
+  toastTimer = setTimeout(() => {
+    toastNode.classList.add('closeToast');
+  }, duration * 1000);
+}
+
+function buildMessage(message, details = '') {
   clearTimeout(toastTimer);
   if (!toastNode) {
     toastNode = document.getElementById('toast') || buildDefaultToast();
@@ -76,13 +85,60 @@ function show(message, details = '', duration = 2) {
       toastDetailNode.hidden = true;
     }
   }
-
-  toastNode.showPopover();
-  toastNode.classList.remove('closeToast');
-
-  toastTimer = setTimeout(() => {
-    toastNode.classList.add('closeToast');
-  }, duration * 1000);
 }
 
-export default { show };
+function message(message, details = '', duration = 2) {
+  buildMessage(message, details);
+  toastNode.classList.remove(
+    'bg-negative-300',
+    'border-negative-600',
+    'text-negative-900',
+    'bg-positive-100',
+    'border-positive-600',
+    'text-positive-900',
+  );
+  toastNode.classList.add(
+    'bg-neutral-100',
+    'border-neutral-600',
+    'text-shark-700',
+  );
+  show(duration);
+}
+
+function success(message, details = '', duration = 2) {
+  buildMessage(message, details);
+  toastNode.classList.remove(
+    'bg-negative-300',
+    'border-negative-600',
+    'text-negative-900',
+    'bg-neutral-100',
+    'border-neutral-600',
+    'text-shark-700',
+  );
+  toastNode.classList.add(
+    'bg-positive-100',
+    'border-positive-600',
+    'text-positive-900',
+  );
+  show(duration);
+}
+
+function warn(message, details = '', duration = 2) {
+  buildMessage(message, details);
+  toastNode.classList.remove(
+    'bg-positive-100',
+    'border-positive-600',
+    'text-positive-900',
+    'bg-neutral-100',
+    'border-neutral-600',
+    'text-shark-700',
+  );
+  toastNode.classList.add(
+    'bg-negative-300',
+    'border-negative-600',
+    'text-negative-900',
+  );
+  show(duration);
+}
+
+export default { message, warn, success };
