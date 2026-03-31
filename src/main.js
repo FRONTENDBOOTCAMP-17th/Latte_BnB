9e5de33796425a36242823891013d9b7a48b6c07
zzz664 <temperis0512@gmail.com>
2026-03-31 10:55:21 +0900

    feat(footer): footer 자동 주입 스크립트 추가
    footer.js - footer요소를 생성하는 buildFooter 함수 추가
    main.js - buildFooter로 생성된 footer를 body 맨 마지막에 추가
    index.html - footer 컴포넌트 제작으로 기존 footer 마크업 제거
    
    resolves #26

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import './style.css';
 import { buildHeader } from './components/header.js';
+import { buildFooter } from './components/footer.js';
 
 document.body.prepend(buildHeader());
+document.body.append(buildFooter());
