511bcf30c6b026e81e528e993f8201565f39204b
zzz664 <temperis0512@gmail.com>
2026-03-31 19:05:26 +0900

    feat(admin/login): admin login페이지 폼 검증 로직 추가
    popover로 빈 칸이 있으면 채울 것을 사용자에게 알림
    popover용 마크업 추가

--- admin/login/index.js
+++ admin/login/index.js
@@ -1,1 +1,46 @@
 import '../../src/style.css';
+
+const adminLoginForm = document.getElementById('adminLoginForm');
+const warnning = document.getElementById('warnning');
+const blankInputMessage = document.querySelector('.blankInputMessage');
+let popoverTimer = null;
+
+warnning.addEventListener('transitionend', (e) => {
+  if (
+    !warnning.classList.contains('closePopover') ||
+    !warnning.matches(':popover-open') ||
+    e.propertyName !== 'opacity'
+  ) {
+    return;
+  }
+  warnning.classList.remove('closePopover');
+  warnning.hidePopover();
+});
+
+adminLoginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let message = [];
+  let showMessage = false;
+
+  clearTimeout(popoverTimer);
+
+  if (!e.target.adminId.value.trim()) {
+    message.push('아이디');
+    showMessage = true;
+  }
+  if (!e.target.adminPassword.value.trim()) {
+    message.push('비밀번호');
+    showMessage = true;
+  }
+
+  if (showMessage) {
+    blankInputMessage.textContent = message.join(', ');
+    warnning.showPopover();
+    warnning.classList.remove('closePopover');
+
+    popoverTimer = setTimeout(() => {
+      warnning.classList.add('closePopover');
+    }, 2000);
+  }
+});
