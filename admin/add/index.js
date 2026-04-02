49781af27080b62d660eaed7ec2f85e38ed56396
zzz664 <temperis0512@gmail.com>
2026-04-02 10:08:54 +0900

    style(admin/add): 마크업, 스타일 수정
    input date의 스타일 수정
    예약 불가 날짜 설정 컨테이너의 디스플레이를 grid로 변경
    input date의 최소 선택 날짜를 오늘로 지정, 과거의 날짜는 선택 불가능

--- admin/add/index.js
+++ admin/add/index.js
@@ -3,4 +3,7 @@ import numberInput from '../../src/components/numberInput';
 
 document.addEventListener('DOMContentLoaded', () => {
   numberInput.render();
+  document.querySelectorAll('input[type=date]').forEach((el) => {
+    el.setAttribute('min', new Date().toISOString().split('T')[0]);
+  });
 });
