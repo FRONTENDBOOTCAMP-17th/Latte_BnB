94eb9958ee85c242de9257eb6b63563fa386dc5c
zzz664 <temperis0512@gmail.com>
2026-04-02 13:50:34 +0900

    feat(admin/add): 썸네일 프리뷰 기능 추가
    썸네일 프리뷰 기능을 추가하면서 이미지추가 버튼(썸네일, 이미지) 두 개의 스타일을 변경
    file input의 값이 변경되면 프리뷰로 이미지를 확인하거나, 이미지를 삭제하거나 수정할 수 있음

--- admin/add/index.js
+++ admin/add/index.js
@@ -1,9 +1,52 @@
 import '../../src/style.css';
 import numberInput from '../../src/components/numberInput';
 
+let uploadThumbnailBtn = null;
+let deleteThumbnailBtn = null;
+let thumbnailInput = null;
+let thumbnailPreview = null;
+let preview = null;
+
+function handleThumbnail(e) {
+  if (e.target.files && e.target.files[0]) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      preview.src = e.target.result;
+      preview.classList.remove('hidden');
+      deleteThumbnailBtn.classList.remove('hidden');
+      uploadThumbnailBtn.className = 'adminThumbnailModify';
+    };
+    reader.readAsDataURL(thumbnailInput.files[0]);
+  } else {
+    preview.src = '';
+    preview.classList.add('hidden');
+    deleteThumbnailBtn.classList.add('hidden');
+    uploadThumbnailBtn.className = 'adminThumbnailUpload';
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   numberInput.render();
   document.querySelectorAll('input[type=date]').forEach((el) => {
     el.setAttribute('min', new Date().toISOString().split('T')[0]);
+  });
+
+  uploadThumbnailBtn = document.getElementById('uploadThumbnail');
+  deleteThumbnailBtn = document.getElementById('deleteThumbnail');
+  thumbnailInput = document.getElementById('thumbnail');
+  thumbnailPreview = document.getElementById('thumbnailPreview');
+  preview = document.getElementById('preview');
+
+  uploadThumbnailBtn.addEventListener('click', () => {
+    thumbnailInput.click();
+  });
+
+  deleteThumbnailBtn.addEventListener('click', () => {
+    thumbnailInput.value = '';
+    thumbnailInput.dispatchEvent(new Event('change', { bubbles: true }));
+  });
+
+  thumbnailInput.addEventListener('change', (e) => {
+    handleThumbnail(e);
   });
 });
