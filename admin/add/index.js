63d31bbc661beb16f76210f9a4176f189d199643
zzz664 <temperis0512@gmail.com>
2026-04-02 17:38:14 +0900

    feat(admin/add): 세부 이미지 추가 및 제목,설명 설정기능 추가
    여러장의 숙소이미지를 추가하면 삭제,수정을 쉽게 할 수 있도록 file과 PreviewImage객체를 Map으로 관리함
    키값은 사진을 불러올때 생성된 랜덤 UUID값이고, data속성으로 DOM노드가 소유함, 이 값으로 Map에 접근
    또한 제목, 설명 추가를 위한 modal추가
    modal이 열릴땐 기존 Map에 저장되어있는 제목, 설명, 객체의 src로 미리보기 이미지를 설정해서 보여줌
    modal이 저장 없이 닫힐 땐, input의 내용을 초기화 함
    modal이 저장 후 닫힐 땐, input의 내용으로 Map에 저장되 내용을 업데이트함

--- admin/add/index.js
+++ admin/add/index.js
@@ -1,28 +1,40 @@
 import '../../src/style.css';
 import numberInput from '../../src/components/numberInput';
+import { PreviewImage } from '../../src/components/previewImage.js';
 
 let uploadThumbnailBtn = null;
 let deleteThumbnailBtn = null;
 let thumbnailInput = null;
 let thumbnailPreview = null;
 let preview = null;
+let uploadImagesBtn = null;
+let imagesInput = null;
+let imagesPreview = null;
+let imageMap = new Map();
+let thumbnailFile = null;
+let modal = null;
+let imageTitle = null;
+let imageDescription = null;
+let selectedImage = null;
 
 function handleThumbnail(e) {
   if (e.target.files && e.target.files[0]) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      preview.src = e.target.result;
-      preview.classList.remove('hidden');
-      deleteThumbnailBtn.classList.remove('hidden');
-      uploadThumbnailBtn.className = 'adminThumbnailModify';
-    };
-    reader.readAsDataURL(thumbnailInput.files[0]);
+    preview.src = URL.createObjectURL(e.target.files[0]);
+    preview.classList.remove('hidden');
+    deleteThumbnailBtn.classList.remove('hidden');
+    uploadThumbnailBtn.className = 'adminThumbnailModify';
   } else {
     preview.src = '';
     preview.classList.add('hidden');
     deleteThumbnailBtn.classList.add('hidden');
     uploadThumbnailBtn.className = 'adminThumbnailUpload';
   }
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  imageTitle.value = '';
+  imageDescription.value = '';
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -36,6 +48,13 @@ document.addEventListener('DOMContentLoaded', () => {
   thumbnailInput = document.getElementById('thumbnail');
   thumbnailPreview = document.getElementById('thumbnailPreview');
   preview = document.getElementById('preview');
+  uploadImagesBtn = document.getElementById('uploadImagesBtn');
+  imagesInput = document.getElementById('images');
+  imagesPreview = document.getElementById('imagesPreview');
+  modal = document.getElementById('modal');
+  imageTitle = document.getElementById('imageTitle');
+  imageDescription = document.getElementById('imageDescription');
+  selectedImage = document.getElementById('selectedImage');
 
   uploadThumbnailBtn.addEventListener('click', () => {
     thumbnailInput.click();
@@ -43,10 +62,61 @@ document.addEventListener('DOMContentLoaded', () => {
 
   deleteThumbnailBtn.addEventListener('click', () => {
     thumbnailInput.value = '';
+    thumbnailFile = null;
     thumbnailInput.dispatchEvent(new Event('change', { bubbles: true }));
   });
 
   thumbnailInput.addEventListener('change', (e) => {
     handleThumbnail(e);
+  });
+
+  uploadImagesBtn.addEventListener('click', () => {
+    imagesInput.click();
+  });
+
+  imagesInput.addEventListener('change', (e) => {
+    if (e.target.files) {
+      const files = Array.from(e.target.files);
+      files.forEach((file) => {
+        const previewImage = new PreviewImage(
+          crypto.randomUUID(),
+          URL.createObjectURL(file),
+        );
+        imageMap.set(previewImage.getId(), {
+          file: file,
+          object: previewImage,
+        });
+        imagesPreview.insertBefore(previewImage.getElement(), uploadImagesBtn);
+      });
+    }
+  });
+
+  document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('deleteImageBtn')) {
+      const target = e.target.closest('div');
+      imageMap.delete(target.dataset.id);
+      target.remove();
+    }
+
+    if (e.target.hasAttribute('data-image')) {
+      const target = e.target.closest('div');
+      const previewImage = imageMap.get(target.dataset.id).object;
+      selectedImage.src = previewImage.getSrc();
+      selectedImage.dataset.id = target.dataset.id;
+      imageTitle.value = previewImage.getTitle();
+      imageDescription.value = previewImage.getDescription();
+      modal.classList.remove('hidden');
+    }
+
+    if (e.target.id === 'apply') {
+      const previewImage = imageMap.get(selectedImage.dataset.id).object;
+      previewImage.setTitle(imageTitle.value);
+      previewImage.setDescription(imageDescription.value);
+      closeModal();
+    }
+
+    if (e.target.id === 'modal') {
+      closeModal();
+    }
   });
 });
