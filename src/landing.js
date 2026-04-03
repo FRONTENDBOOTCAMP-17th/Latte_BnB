4f371590cb8021de5248ac782da92775c4e528c7
zzz664 <temperis0512@gmail.com>
2026-04-03 13:54:41 +0900

    feat(landing): api 호출 방식 변경 및 검색, 정렬에 대한 로직 추가
    기본적으로 sort와 pageLimit을 포함하도록 변경
    page를 불러올땐 page도 포함
    검색할땐 query를 포함
    검색을 하거나, 정렬방법을 변경했을 경우 현재 페이지는 1로 다시 초기화 함
    검색 시 엔터를 눌러도 검색버튼의 click 이벤트가 발생하도록 함
    검색 버튼을 누르면 fetchSearch를 통해 검색된 목록을 가져옴
    정렬 방식 선택박스의 change 이벤트가 감지되면 기본 가져오기를 수행함.
    
    resolves #56

--- src/landing.js
+++ src/landing.js
@@ -2,15 +2,21 @@ import { RoomCard } from './RoomCard.js';
 import constants from './constants.js';
 import pagination from './components/pagination.js';
 
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+const sortBox = document.getElementById('sort');
 const content = document.getElementById('content');
 const roomList = document.getElementById('roomList');
 let roomData = new Map();
 let pageLimit = 20;
 
 async function fetchRooms() {
-  const res = await fetch(`${constants.API_BASE_URL}/accommodations`, {
-    method: 'GET',
-  });
+  const res = await fetch(
+    `${constants.API_BASE_URL}/accommodations?pageLimit=${pageLimit}&sort=${sortBox.value}`,
+    {
+      method: 'GET',
+    },
+  );
 
   const { message, data, meta } = await res.json();
 
@@ -23,7 +29,24 @@ async function fetchRooms() {
 
 async function fetchPage() {
   const res = await fetch(
-    `${constants.API_BASE_URL}/accommodations?page=${pagination.paginationData.page}&pageLimit=${pageLimit}`,
+    `${constants.API_BASE_URL}/accommodations?page=${pagination.paginationData.page}&pageLimit=${pageLimit}&sort=${sortBox.value}`,
+    {
+      method: 'GET',
+    },
+  );
+
+  const { message, data, meta } = await res.json();
+
+  if (!res.ok) {
+    throw new Error(message);
+  }
+
+  return { data, meta };
+}
+
+async function fetchSearch(query) {
+  const res = await fetch(
+    `${constants.API_BASE_URL}/accommodations?pageLimit=${pageLimit}&query=${query}&sort=${sortBox.value}`,
     {
       method: 'GET',
     },
@@ -65,6 +88,12 @@ renderRooms();
 
 content.appendChild(pagination.buildPagination(result.meta.pagination));
 
+searchInput.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    searchBtn.click();
+  }
+});
+
 document.addEventListener('click', async (e) => {
   if (e.target.id === 'prevButton') {
     pagination.setCurrentPage(pagination.paginationData.page - 1);
@@ -74,5 +103,23 @@ document.addEventListener('click', async (e) => {
   if (e.target.id === 'nextButton') {
     pagination.setCurrentPage(pagination.paginationData.page + 1);
     changePage();
+  }
+
+  if (e.target.id === 'searchBtn') {
+    const result = await fetchSearch(searchInput.value);
+    pagination.setCurrentPage(result.meta.pagination.page);
+    buildRooms(result.data.accommodations);
+    renderRooms();
+    pagination.setPrevNext(result.meta.pagination);
+  }
+});
+
+document.addEventListener('change', async (e) => {
+  if (e.target.id === 'sort') {
+    const result = await fetchRooms();
+    pagination.setCurrentPage(result.meta.pagination.page);
+    buildRooms(result.data.accommodations);
+    renderRooms();
+    pagination.setPrevNext(result.meta.pagination);
   }
 });
