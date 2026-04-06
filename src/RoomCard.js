c557b0f6ee5f8f8f59684110a97a70b0bfaa066f
zzz664 <temperis0512@gmail.com>
2026-04-06 11:20:50 +0900

    feat(wish): 카드의 하트를 누르면 찜 토글이 가능하도록 기능 추가
    토스트를 이용하여 성공, 실패를 알려준다.
    성공시 찜 해제인지, 찜 추가인지 알려준다.

--- src/RoomCard.js
+++ src/RoomCard.js
@@ -1,3 +1,6 @@
+import constants from './constants.js';
+import toast from './components/toast.js';
+
 export class RoomCard {
   #id;
   #title;
@@ -5,12 +8,14 @@ export class RoomCard {
   #thumbnailUrl;
   #pricing;
   #maxGuest;
+  #isWish = false;
   #element = null;
   #titleNode = null;
   #thumbnailNode = null;
   #priceNode = null;
   #maxGuestNode = null;
   #locationNode = null;
+  #wishNode = null;
 
   constructor({
     id,
@@ -35,39 +40,63 @@ export class RoomCard {
     li.dataset.id = this.#id;
 
     li.innerHTML = `
-    <a href="/accommodations-detail/?id=${this.#id}">
-      <div class="min-w-[320px] w-full h-full bg-slate-50 text-shark-800 rounded-xl overflow-clip shadow-lg cursor-pointer ">
-        <img src='${this.#thumbnailUrl}' alt='대표 이미지' class="accommodationThumbnail w-full h-46 object-cover"/>
-        <div class="p-4 pt-2 grid grid-cols-2 grid-rows-[repeat(3,minmax(56px,1fr))] gap-1">
-          <p class="accommodationTitle text-lg col-satrt-1"></p>
-          <p class="accommodationPrice text-xl font-semibold col-start-2 justify-self-end">
-          </p>
-          <p class="accommodationMaxGuest text-sm row-start-3 col-start-1 self-start"></p>
-          <p class="accommodationLocation text-sm row-start-3 col-start-1 self-end"></p>
-          <div
-            class="text-transparent col-start-2 row-start-3 place-self-end hover:scale-110 transition-[scale] duration-300">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              stroke-width="1"
-              stroke="black"
-              class="size-6">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-            </svg>
+      <div class="min-w-[320px] w-full h-full relative bg-slate-50 text-shark-800 rounded-xl overflow-clip shadow-lg cursor-pointer ">
+        <a href="/accommodations-detail/?id=${this.#id}">
+          <img src='${this.#thumbnailUrl}' alt='대표 이미지' class="accommodationThumbnail w-full h-46 object-cover"/>
+          <div class="p-4 pt-2 grid grid-cols-2 grid-rows-[repeat(3,minmax(56px,1fr))] gap-1">
+            <p class="accommodationTitle text-lg col-satrt-1"></p>
+            <p class="accommodationPrice text-xl font-semibold col-start-2 justify-self-end">
+            </p>
+            <p class="accommodationMaxGuest text-sm row-start-3 col-start-1 self-start"></p>
+            <p class="accommodationLocation text-sm row-start-3 col-start-1 self-end"></p>
+
           </div>
+        </a>
+        <div class="wishHeart absolute text-transparent right-4 bottom-4 hover:scale-110 transition-[scale] duration-300 z-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            stroke-width="1"
+            stroke="black"
+            class="size-6">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+          </svg>
         </div>
-      </div>
-    </a>`;
+      </div>`;
 
     this.#titleNode = li.querySelector('.accommodationTitle');
     this.#thumbnailNode = li.querySelector('.accommodationThumbnail');
     this.#priceNode = li.querySelector('.accommodationPrice');
     this.#locationNode = li.querySelector('.accommodationLocation');
     this.#maxGuestNode = li.querySelector('.accommodationMaxGuest');
+    this.#wishNode = li.querySelector('.wishHeart');
+    this.#wishNode.addEventListener('click', async (e) => {
+      const res = await fetch(`${constants.API_BASE_URL}/me/wishlist`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        },
+        body: JSON.stringify({
+          accommodationId: this.#id,
+          isWishlisted: !this.#isWish,
+        }),
+      });
+
+      const result = await res.json();
+
+      if (!result.success) {
+        toast.warn('실패', '위시리스트에 추가하려면 로그인해야합니다.', 3);
+        return;
+      }
+
+      this.setWish(result.data.isWishlisted);
+      toast.success('성공', result.message, 3);
+    });
 
     this.#syncDOM();
 
@@ -100,5 +129,17 @@ export class RoomCard {
     this.#pricing = pricing;
     this.#thumbnailUrl = tumbnailUrl;
     this.#syncDOM();
+  }
+
+  setWish(isWish) {
+    this.#isWish = isWish;
+
+    if (this.#isWish) {
+      this.#wishNode.classList.remove('text-transparent');
+      this.#wishNode.classList.add('text-primary-400');
+    } else {
+      this.#wishNode.classList.add('text-transparent');
+      this.#wishNode.classList.remove('text-primary-400');
+    }
   }
 }
