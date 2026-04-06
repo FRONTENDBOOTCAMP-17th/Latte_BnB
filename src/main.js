c0a2f1e876a62802e54cd8dfff858013e8f02fb7
zzz664 <temperis0512@gmail.com>
2026-04-06 13:49:24 +0900

    fix(nav,header,footer): 공용 컴포넌트 사용여부 추가 및 컴포넌트 분리
    header에 묶여있던 nav를 개별적으로 선택해서 추가할 수 있도록 변경함
    header, nav, footer의 사용여부는 body태그에 data속성으로 data-header, data-footer, data-nav 속성의 값을 true, false로 주어 사용여부를 정할 수 있음
    
    resolves #85

--- src/main.js
+++ src/main.js
@@ -1,6 +1,63 @@
 import './style.css';
 import { buildHeader } from './components/header.js';
 import { buildFooter } from './components/footer.js';
+import hamburger from './components/hamburger';
+import navigation from './components/navigation';
+import constants from './constants.js';
 
-document.body.prepend(buildHeader());
-document.body.append(buildFooter());
+let result = null;
+
+if (document.body.dataset.header === 'true') {
+  document.body.prepend(buildHeader());
+
+  document.querySelector('header').appendChild(hamburger.buildHamburger());
+
+  result = await checkAuth();
+
+  document.querySelector('.hamburger').appendChild(hamburger.buildMenu(result));
+  hamburger.attachEvent();
+}
+
+if (document.body.dataset.nav === 'true') {
+  if (result === null) {
+    result = await checkAuth();
+  }
+  document.body.appendChild(navigation.buildNavigation());
+  navigation.buildMenu(result);
+}
+
+if (document.body.dataset.footer === 'true') {
+  document.body.append(buildFooter());
+}
+
+async function checkAuth() {
+  const token = localStorage.getItem('accessToken');
+
+  try {
+    const data = await getProfile(token);
+
+    if (data !== null) {
+      return { isAuth: true, data };
+    }
+    return { isAuth: false, data: null };
+  } catch (error) {
+    return { isAuth: false, data: null };
+  }
+}
+
+async function getProfile(token) {
+  const res = await fetch(`${constants.API_BASE_URL}/me/profile`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const { message, data } = await res.json();
+
+  if (!res.ok) {
+    throw new Error(message);
+  }
+
+  return data;
+}
