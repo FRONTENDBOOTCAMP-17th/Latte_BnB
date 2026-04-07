6660d68beb94adf00bd0879cfb4d013e0e5e9b9a
lllillly <qkdgywls0503@naver.com>
2026-04-07 11:12:43 +0900

    feat(navigaion-ui): nav바 design 수정 및 img 태그 svg 변경

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,9 +1,11 @@
 let navigation = null;
 
+const path = location.pathname;
+
 function buildNavigation() {
   const div = document.createElement('div');
   div.className =
-    'flex lg:hidden justify-between px-[clamp(1.25rem,4vw,5rem)] pt-2 pb-1 border-t-2 border-shark-300 bg-shark-100 min-w-100 w-full fixed bottom-0';
+    'flex lg:hidden justify-around px-2 pt-2 pb-3 bg-white w-full fixed bottom-0 shadow-[0_-3px_14px_rgba(0,0,0,0.08)]';
 
   navigation = div;
   return div;
@@ -12,29 +14,29 @@ function buildNavigation() {
 function buildMenu(result) {
   if (result.isAuth) {
     navigation.innerHTML = `
-    <a href="/" class="flex flex-col items-center justify-center text-sm">
-      <img src="/src/assets/search.svg" alt="nav search svg" class="w-6" />검색
+    <a href="/" class="flex flex-col items-center justify-center gap-1 text-[10px] font-medium ${path === '/' ? 'text-primary-500' : 'text-shark-400'}">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" width="24" height="24"><path d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>검색
     </a>
-    <a href="/wishlist/" class="flex flex-col items-center justify-center text-sm">
-      <img src="/src/assets/wish.svg" alt="nav wish svg" class="w-6" />위시리스트
+    <a href="/wishlist/" class="flex flex-col items-center justify-center gap-1 text-[10px] font-medium ${path === '/wishlist/' ? 'text-primary-500' : 'text-shark-400'}">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24" height="24"><path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" /></svg>위시리스트
     </a>
-    <a href="/reservations-check/" class="flex flex-col items-center justify-center text-sm">
-      <img src="/src/assets/reservation.svg" alt="nav reservation svg" class="w-6" />내 예약
+    <a href="/reservations-check/" class="flex flex-col items-center justify-center gap-1 text-[10px] font-medium ${path === '/reservations-check/' ? 'text-primary-500' : 'text-shark-400'}">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24" height="24"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" /></svg>내 예약
     </a>
-    <a href="/profile/" class="flex flex-col items-center justify-center text-sm">
-      <img src="/src/assets/profile.svg" alt="nav profile svg" class="w-6" />프로필
+    <a href="/profile/" class="flex flex-col items-center justify-center gap-1 text-[10px] font-medium ${path === '/profile/' ? 'text-primary-500' : 'text-shark-400'}">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24" height="24"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" /></svg>프로필
     </a>
     `;
   } else {
     navigation.innerHTML = `
-    <a href="/" class="flex flex-col items-center justify-center text-sm">
-      <img src="/src/assets/search.svg" alt="nav search svg" class="w-6" />검색
+    <a href="/" class="flex flex-col items-center justify-center gap-1 text-[10px] font-medium ${path === '/' ? 'text-primary-500' : 'text-shark-400'}">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" width="24" height="24"><path d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>검색
     </a>
-    <a href="/wishlist/" class="flex flex-col items-center justify-center text-sm">
-      <img src="/src/assets/wish.svg" alt="nav wish svg" class="w-6" />위시리스트
+    <a href="/wishlist/" class="flex flex-col items-center justify-center gap-1 text-[10px] font-medium ${path === '/wishlist/' ? 'text-primary-500' : 'text-shark-400'}">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24" height="24"><path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" /></svg>위시리스트
     </a>
-    <a href="/login/" class="flex flex-col items-center justify-center text-sm">
-      <img src="/src/assets/profile.svg" alt="nav profile svg" class="w-6" />로그인
+    <a href="/login/" class="flex flex-col items-center justify-center gap-1 text-[10px] font-medium ${path === '/login/' ? 'text-primary-500' : 'text-shark-400'}">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24" height="24"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" /></svg>로그인
     </a>
     `;
   }
